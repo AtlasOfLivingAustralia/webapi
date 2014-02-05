53d439a7857cf2aa16eb7c8032ae506b51f1685e
djtfmartin <djtfmartin@gmail.com>
2014-02-05 05:57:31 +0000

    output format
    git-svn-id: http://ala.googlecode.com/svn/trunk/webapi@168 cf76a52e-83f7-1f8e-e3a8-b8364d34af1a

--- grails-app/domain/au/org/ala/webapi/WebService.groovy
+++ grails-app/domain/au/org/ala/webapi/WebService.groovy
@@ -56,7 +56,7 @@ class WebService {
 
     static httpMethods = ['GET','POST','PUT','DELETE']
 
-    static format = ['json', 'png', 'csv', 'zip', 'xml', 'jpg', 'kml', 'shp', 'gzip', 'eml', 'rif-cs','rdf+xml']
+    static format = ['json', 'png', 'csv', 'zip', 'xml', 'jpg', 'kml', 'shp', 'gzip', 'eml', 'rif-cs','rdf+xml','text', 'wkt']
 
     static constraints = {
         name(nullable:false)
